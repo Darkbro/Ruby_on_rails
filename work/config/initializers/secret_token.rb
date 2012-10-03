b8c5363f6e116bdb4c350b255095379ebf8e290f
root <root@localhost.localdomain>
2012-10-03 19:37:16 +0800

    ruby_on_rails

---
+++ work/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Work::Application.config.secret_token = 'c86529796641862c8a272012e4010e665e201a0a0f666933fb971c9197e99687ebdd7d13c92039982be95dcf5d4ccbd3f9d24e6ad7847673f91d29855fd3ab85'
